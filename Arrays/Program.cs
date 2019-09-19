2888d226226d656dc469e02cadcb5ff2260d5763
chris <chris@LAPTOP-T9OFFMSV>
2019-09-19 17:51:19 -0400

    <added comments>

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Christopher Boehm
+//Arrays
+//9/19/2019
+using System;
 
 namespace Arrays
 {
@@ -10,6 +9,22 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            //creates an array for 25 values
+            int[] Array = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+            //for loop that iterates through the array and writes to screen each value
+            for (int i=1; i<26; i++)
+            {  
+                Console.WriteLine("Element value = " + Array[i]);
+
+                //if statement that lets the user know the iteration process is complete
+                if (Array[i] == 25)
+                {
+                    Console.WriteLine("Your array interations have ended...");
+                }
+            }
+
+            Console.ReadKey(true);
         }
     }
 }
